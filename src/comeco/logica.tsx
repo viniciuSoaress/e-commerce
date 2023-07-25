6762137b00519034fb5be18841b486dc3affabff
viniciszs <114860862+viniciszs@users.noreply.github.com>
2023-07-24 22:16:55 -0300

    tentando completar a parte da logica

--- src/comeco/logica.tsx
+++ src/comeco/logica.tsx
@@ -2,18 +2,18 @@ import { MouseEventHandler, useState } from "react"
 
 
 const prods = [
-  { id: 0, name: 'arroz', preco: 5.50, descricao: 'alimento' },
-  { id: 1, name: 'fejao', preco: 8, descricao: 'alimento' },
-  { id: 2, name: 'sabao', preco: 26, descricao: 'limpesa' },
-  { id: 3, name: 'vasolra', preco: 12, descricao: 'limpesa' },
-  { id: 4, name: 'detegente', preco: 1.99, descricao: 'limpesa' },
-  { id: 5, name: 'ovos', preco: 22, descricao: 'alimento' },
-  { id: 6, name: 'macarrao', preco: 3.24, descricao: 'alimento' },
-  { id: 7, name: 'manteiga', preco: 8.54, descricao: 'alimento' },
-  { id: 8, name: 'queijo', preco: 85, descricao: 'alimento' },
-  { id: 9, name: 'farinha', preco: 4, descricao: 'alimento' },
-  { id: 10, name: 'batata', preco: 2.8, descricao: 'alimento' },
-  { id: 11, name: 'cebola', preco: 3, descricao: 'alimento' },
+  { id: 0, name: 'arroz', preco: 5.50, descricao: 'alimento', quant: 0 },
+  { id: 1, name: 'fejao', preco: 8, descricao: 'alimento', quant: 0 },
+  { id: 2, name: 'sabao', preco: 26, descricao: 'limpesa', quant: 0 },
+  { id: 3, name: 'vasolra', preco: 12, descricao: 'limpesa', quant: 0 },
+  { id: 4, name: 'detegente', preco: 1.99, descricao: 'limpesa', quant: 0 },
+  { id: 5, name: 'ovos', preco: 22, descricao: 'alimento', quant: 0 },
+  { id: 6, name: 'macarrao', preco: 3.24, descricao: 'alimento', quant: 0 },
+  { id: 7, name: 'manteiga', preco: 8.54, descricao: 'alimento', quant: 0 },
+  { id: 8, name: 'queijo', preco: 85, descricao: 'alimento', quant: 0 },
+  { id: 9, name: 'farinha', preco: 4, descricao: 'alimento', quant: 0 },
+  { id: 10, name: 'batata', preco: 2.8, descricao: 'alimento', quant: 0 },
+  { id: 11, name: 'cebola', preco: 3, descricao: 'alimento', quant: 0 },
 
 ]
 
@@ -23,6 +23,7 @@ type Products = {
   name: string,
   preco: number,
   descricao: string,
+  quant: number
 }
 
 export function Inplementacao() {
@@ -34,19 +35,23 @@ export function Inplementacao() {
   const [p, setP] = useState<Products>({} as Products);
 
 
+  let valor = 0;
+
+
   function handleIsCart(it: Products) {
     setIsCart(ic => !ic)
-    setP({ id: it.id, name: it.name, preco: it.preco, descricao: it.descricao })
+    setP({ id: it.id, name: it.name, preco: it.preco, descricao: it.descricao, quant: it.quant })
   }
 
-  function handleAddClick(prod: Products) {
+  function handleAddClick(prod: Products, value: number) {
     setProducts([
       ...products,
       {
         id: prod.id,
         name: prod.name,
         preco: prod.preco,
-        descricao: prod.descricao
+        descricao: prod.descricao,
+        quant: value,
       }
     ])
   }
@@ -61,7 +66,7 @@ export function Inplementacao() {
           <Produtos
             key={p.id}
             p={p}
-            onClick={() => handleAddClick(p)}
+            onClick={handleAddClick}
             onClickVisible={() => handleIsCart(p)}
           />
         ))}
@@ -78,7 +83,8 @@ export function Inplementacao() {
           </p>
         ))} */}
       </main>
-        <Sacola p={products}/>
+      {products.map(i => { valor += i.preco })}
+      <Sacola p={products} value={valor} />
 
       {isCart && (<Prod produtos={p} onClick={() => setIsCart(false)} />)}
     </>
@@ -91,12 +97,47 @@ type ProductsProps = {
     name: string,
     preco: number,
     descricao: string,
+    id: number, 
+    quant: number,
   }
-  onClick: MouseEventHandler<HTMLButtonElement>,
+  onClick: (prod: Products, value: number) => void,
   onClickVisible: MouseEventHandler<HTMLButtonElement>
 }
 
 function Produtos({ p, onClick, onClickVisible }: ProductsProps) {
+
+  const [isShow, setIsShow] = useState(false);
+
+  const [quant, setQuant] = useState(0)
+
+
+  function handleIsShowClick() {
+    setIsShow(true)
+  }
+
+  if (isShow) {
+    return (
+      <div style={{ border: '1px solid ' }}>
+
+        <p>nome: {p.name}</p>
+        <small>preço: {p.preco}$</small>
+        <br />
+        <small>descrição: {p.descricao}</small>
+        <br />
+        <button onClick={() => setQuant(q => q + 1)}>
+          adicionar
+        </button>
+        <button onClick={() => quant > 0 && setQuant(q => q - 1)}>
+          remover
+        </button>
+        <button onClick={() => onClick(p, quant)}>
+          conprar
+        </button>
+
+        {quant}
+      </div>
+    )
+  }
 
   return (
     <div style={{ border: '1px solid ' }}>
@@ -106,8 +147,11 @@ function Produtos({ p, onClick, onClickVisible }: ProductsProps) {
       <br />
       <small>descrição: {p.descricao}</small>
       <br />
-      <button onClick={onClick}>
-        comprar
+      <button onClick={(e) => {
+        handleIsShowClick();
+        // onClick(e)
+      }}>
+        adicionar produto
       </button>
       <button onClick={onClickVisible}>
         ver produto
@@ -115,6 +159,7 @@ function Produtos({ p, onClick, onClickVisible }: ProductsProps) {
     </div>
   )
 }
+
 
 type ProdProps = {
   produtos: Products,
@@ -137,26 +182,28 @@ function Prod({ produtos, onClick }: ProdProps) {
 
 
 type SacolaProps = {
-  p: Products[]
+  p: Products[],
+  value: number,
 }
-function Sacola({ p }: SacolaProps) {
+function Sacola({ p, value }: SacolaProps) {
 
-  
+
 
   return (
-    <div style={{ zIndex: 1, top: 0, right: 0}}>
+    <div style={{ zIndex: 1, top: 0, right: 0 }}>
       <h2>{p.length}</h2>
       <ul>
         {p.map(d => (
           <li key={d.id}>
             {d.name}
             {d.preco}
-            
+            {d.quant}
+
           </li>
-      ))}
+        ))}
       </ul>
 
-      <h4>preco:{}</h4>
+      <h4>preco:{value.toFixed(2)}$</h4>
     </div>
   )
 }
